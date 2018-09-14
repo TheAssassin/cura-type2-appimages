a730240fe1890677d7ca6f9d2de7c052c803f7ab
TheAssassin <theassassin@assassinate-you.net>
2018-09-14 03:04:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cura-type2-appimages
+
+This tool "converts" Cura AppImages from type 1 into type 2, which is a lot more modern, more efficient and has better support in tools. The tool is run automatically on Travis CI, uploading the resulting AppImages on GitHub releases. Furthermore, update information is embedded, so users can conveniently upgrade to newer releases which have been built in this repository.
+
+The contents of the AppImage are exactly the same as the ones in the [official Cura AppImages](https://github.com/Ultimaker/Cura/releases), please see their page for details.
+
+This tool is MIT licensed. See [LICENSE](LICENSE.txt) for more information.
---
+++ convert.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+set -xe
+
+# if this is a tag build, we can use the tag name as the version
+if [ "$TRAVIS_TAG" != "" ]; then
+    export VERSION="$TRAVIS_TAG"
+fi
+
+# if $VERSION is set, build AppImage for that specific version, otherwise use latest version
+URLS=$(curl -s https://api.github.com/repos/Ultimaker/Cura/releases | grep browser_download_url | cut -d: -f2- | cut -d'"' -f2 | grep -E '.AppImage$')
+if [ "$VERSION" == "" ]; then
+    URL=$(echo "$URLS" | head -n1)
+    export VERSION=$(echo "$URL" | python3 -c "import re, sys; print(re.search('Cura-([\d\.]+)\.AppImage', sys.stdin.read()).group(1))")
+else
+    URL=$(echo "$URLS" | grep "$VERSION" | head -n1)
+fi
+
+if [ "$URL" == "" ]; then
+    if [ "$VERSION" != "" ]; then
+        echo "Error: could not find URL for user-specified version $VERSION"
+    else
+        echo "Error: could not determine URL for latest version"
+    fi
+    exit 1
+fi
+
+# use RAM disk if possible
+if [ "$CI" == "" ] && [ -d /dev/shm ]; then
+    TEMP_BASE=/dev/shm
+else
+    TEMP_BASE=/tmp
+fi
+
+BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" cura-type2-appimages-build-XXXXXX)
+
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+
+trap cleanup EXIT
+
+# store repo root as variable
+REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
+OLD_CWD=$(readlink -f .)
+
+pushd "$BUILD_DIR"
+
+wget -c "$URL"
+
+xorriso -indev Cura-*.AppImage -osirrox on -extract / AppDir
+
+wget -c https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
+
+export UPD_INFO="gh-releases-zsync|TheAssassin|cura-type2-appimages|latest|Cura-*.AppImage"
+./appimagetool-x86_64.AppImage -u "$UPD_INFO" AppDir
+
+mv Cura*.AppImage "$OLD_CWD"
